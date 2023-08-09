50836bb7fb64fcb5cd5c1cb920384f77f949c145
Alexey Shapiro <academ@gmail.com>
2023-08-09 18:07:22 +0300

    migrated to getting settings from a microservice

--- watcher.py
+++ watcher.py
@@ -1,32 +1,34 @@
 import asyncio
-import os
-from dotenv import load_dotenv
-import redis.asyncio as redis
+import logging
 from pickle import dumps
+
+import redis.asyncio as redis
 from tinkoff.invest import AsyncClient, OrderTrades
 from tinkoff.invest.exceptions import AioRequestError
-import logging
 
-load_dotenv()
+import settings
+
+
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG
 )
+trades_url = settings.get('TRADES_URL')
 
 
 async def save_tcs_trade(trades: OrderTrades):
     logging.info(f'received new trades: {trades}')
-    async with redis.from_url(os.getenv('REDIS_URL')) as client:
+    async with redis.from_url(trades_url) as client:
         r = await client.lpush('tcs_trades', dumps(trades))
     if not r:
         logging.error(f'could not save trades: {r}, {trades}')
 
 
 async def watch_tcs_orders():
-    async with AsyncClient(os.getenv('TCS_RO_TOKEN')) as client:
+    async with AsyncClient(settings.get('TCS_RO_TOKEN')) as client:
         while True:
             try:
                 async for trades in client.orders_stream.trades_stream(
-                    accounts=[os.getenv('TCS_ACCOUNT_ID')]
+                    accounts=[settings.get('TCS_ACCOUNT_ID')]
                 ):
                     if trades.ping is not None:
                         last_ping_time = trades.ping.time
